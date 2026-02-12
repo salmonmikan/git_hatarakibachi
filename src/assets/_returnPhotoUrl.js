567440cee4c289eb7be03ed23bd82641c99c2bd6
y.ebara <yuki2001yuki@gmail.com>
2026-02-12 21:14:22 +0900

    image_resizeによる画像サイズ最適化

--- src/assets/_returnPhotoUrl.js
+++ src/assets/_returnPhotoUrl.js
@@ -1,3 +1,3 @@
-export function returnPhotoUrl(src) {
-    return src ? `https://public.hatarakibachi.com/${src}` : '';
+export function returnPhotoUrl(photoUrl, size, gravity) {
+    return photoUrl ? `https://public.hatarakibachi.com/cdn-cgi/image/w=${size},h=${size},gravity=${gravity},fit=cover,f=auto,q=80/${photoUrl}` : '';
 };
